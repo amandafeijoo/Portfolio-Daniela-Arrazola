b011c6c411f279ac850b5d6e4b6ffaeaf99cdad9
Amanda <amanda_feijoo@hotmail.com>
2025-04-23 16:00:19 +0200

    🔧 Fix: Corrige ruta de build del frontend y copia en Docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,11 +9,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// export default defineConfig({
+//   base: '/static/',
+//   plugins: [react()],
+//   build: {
+//     outDir: '../frontend_build', 
+//     emptyOutDir: true,
+//   },
+// })
+
+
 export default defineConfig({
   base: '/static/',
   plugins: [react()],
   build: {
-    outDir: '../frontend_build', // o 'dist', como prefieras
+    outDir: 'dist', // 💡 Genera la build dentro del mismo directorio del frontend
     emptyOutDir: true,
   },
 })

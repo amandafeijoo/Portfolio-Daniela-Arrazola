ccd2459642f96ef480f54d7b1c11a7d9a6e7f7ee
Amanda <amanda_feijoo@hotmail.com>
2025-04-23 10:56:52 +0200

    ⚙️ Updated build and copiado imágenes y videos públicos

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,26 @@
 # echo "git push"
 
 
+# #!/bin/bash
+
+# echo "🧼 Borrando archivos antiguos..."
+# rm -rf staticfiles/*
+
+# echo "🛠️  Generando build del frontend..."
+# cd frontend || exit
+# npm run build
+# cd ..
+
+# echo "📦 Copiando archivos del build..."
+# cp -r frontend_build/assets staticfiles/
+# cp -r frontend_build/images staticfiles/
+# cp frontend_build/index.html staticfiles/
+
+# echo "✅ Build actualizado y copiado en staticfiles"
+# echo "Recuerda hacer:"
+# echo "git add . && git commit -m '⚙️ Updated frontend build and forced static refresh' && git push"
+
+
 #!/bin/bash
 
 echo "🧼 Borrando archivos antiguos..."
@@ -30,6 +50,10 @@ echo "📦 Copiando archivos del build..."
 cp -r frontend_build/assets staticfiles/
 cp -r frontend_build/images staticfiles/
 cp frontend_build/index.html staticfiles/
+
+echo "📂 Copiando imágenes y videos públicos..."
+cp -r frontend/public/images staticfiles/
+cp -r frontend/public/videos staticfiles/
 
 echo "✅ Build actualizado y copiado en staticfiles"
 echo "Recuerda hacer:"
